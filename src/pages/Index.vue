50412c6e5d779cd44f86128106c6934ba12ab508
Jing Dong <jing@192-168-1-5.tpgi.com.au>
2019-04-22 23:10:11 +1000

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- <img alt="Quasar logo" src="~assets/quasar-logo-full.svg"> -->
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>

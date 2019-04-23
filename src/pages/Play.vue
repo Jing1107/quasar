d47832ab7ce59d4358e90fcca6c226c4b7694b68
Jing Dong <jing@192-168-1-5.tpgi.com.au>
2019-04-23 21:43:15 +1000

    2

--- src/pages/Play.vue
+++ src/pages/Play.vue
@@ -1,12 +1,25 @@
 <template>
-    <q-page padding>
-        <div class="row">
-            <div class="col">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, cupiditate cumque! Expedita iusto molestias sequi in eveniet alias inventore temporibus pariatur! At ratione corrupti, deserunt alias aspernatur provident eius dignissimos!
-            </div>
-            <div class="col">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, cupiditate cumque! Expedita iusto molestias sequi in eveniet alias inventore temporibus pariatur! At ratione corrupti, deserunt alias aspernatur provident eius dignissimos!
-            </div>
-        </div>
-    </q-page>
+  <q-page padding>
+  <div class="row">
+    <div class="col">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, cupiditate cumque! Expedita iusto molestias sequi in eveniet alias inventore temporibus pariatur! At ratione corrupti, deserunt alias aspernatur provident eius dignissimos!
+      <div>
+        <q-btn color="primary" label="Primary" @click="handleClick" />
+      </div>
+    </div>
+    <div class="col">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, cupiditate cumque! Expedita iusto molestias sequi in eveniet alias inventore temporibus pariatur! At ratione corrupti, deserunt alias aspernatur provident eius dignissimos!
+    </div>
+  </div>
+  </q-page>
 </template>
+
+<script>
+export default {
+  methods: {
+    handleClick () {
+      alert('hello world')
+    }
+  }
+}
+</script>

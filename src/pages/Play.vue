0f05adbc34a6cd6c02f8d93bf3154bfe006851e8
Jing Dong <jing@192-168-1-5.tpgi.com.au>
2019-04-23 20:51:40 +1000

    add play.vue

--- src/pages/Play.vue
+++ src/pages/Play.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
+    <q-page padding>
         <div class="row">
             <div class="col">
-                .col
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, cupiditate cumque! Expedita iusto molestias sequi in eveniet alias inventore temporibus pariatur! At ratione corrupti, deserunt alias aspernatur provident eius dignissimos!
             </div>
             <div class="col">
-                .col
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, cupiditate cumque! Expedita iusto molestias sequi in eveniet alias inventore temporibus pariatur! At ratione corrupti, deserunt alias aspernatur provident eius dignissimos!
             </div>
         </div>
-    </div>       
+    </q-page>
 </template>
